bc15a18c79d9a880941b8b96794e21666caa0b75
JosephHo290 <JosephHo290@yahoo.com.tw>
2017-08-21 08:02:29 +0800

    God bless us! ...

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+
+	os.path.join(BASE_DIR, 'ststic'),
+	
+	]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
 
 #SECURE_SSL_REDIRECT = True
