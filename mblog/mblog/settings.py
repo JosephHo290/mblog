77fc54805efb34619c465b594330278d730ad535
JosephHo290 <JosephHo290@yahoo.com.tw>
2017-08-21 10:07:20 +0800

    AWSOME

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = 	[
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #SECURE_SSL_REDIRECT = True
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config()
-}
+#import dj_database_url
+#DATABASES = {
+#    'default': dj_database_url.config()
+#}
